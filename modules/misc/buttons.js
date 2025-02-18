82796d9b628e314b72124da078277e0a00c34ec5
Daniel <2006DW1@gmail.com>
2025-02-18 12:43:44 -0500

    added a button to switch between skills and match field

---
+++ modules/misc/buttons.js
@@ -0,0 +1,10 @@
+function switchToSkills() {
+    let element = document.getElementById("canvas");
+    element.style.backgroundImage = "url('./assets/images/high-stakes-skills-field.png')";
+}
+
+  
+function switchToGame() {
+    let element = document.getElementById("canvas");
+    element.style.backgroundImage = "url('./assets/images/high-stakes-field.png')";
+}
