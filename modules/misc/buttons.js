b2b132c8ce488aa6b1ea6663c246b8220f653f5a
Daniel <danielwood0606@gmail>
2025-09-14 21:08:45 -0400

    Added vex u field

--- modules/misc/buttons.js
+++ modules/misc/buttons.js
@@ -8,3 +8,7 @@ function switchToGame() {
     let element = document.getElementById("canvas");
     element.style.backgroundImage = "url('./assets/images/push-back-field.png')";
 }
+function switchToVexu() {
+    let element = document.getElementById("canvas");
+    element.style.backgroundImage = "url('./assets/images/push-back-vexu-field.png')";
+}
